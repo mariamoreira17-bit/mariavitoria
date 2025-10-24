07321b20607f8ee48488d6bfe8429c75aa060d0a
mariamoreira17-bit <maria.moreira17@escola.pr.gov.br>
2025-10-24 10:25:54 -0300

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,89 @@
+Python
+nome: str = "Maria"
+idade: int = 16
+altura: float = 1,65
+ativo: bool = True
+
+JavaScript
+deixe nome = "Maria"; // string
+seja idade = 16; // número (inteiro)
+deixe altura = 1,65; // número (decimal)
+deixe ativo = true; // booleano
+
+Python
+a + b # soma
+a - b # subtração
+a * b # multiplicação
+a / b # divisão
+# Operadores lógicos:
+a e b
+não um
+a ou b
+
+JavaScript
+a + b // soma
+a - b // subtração
+a * b // multiplicação
+a / b // divisão
+// Operadores lógicos:
+um e b
+!um
+um || b
+
+Python
+se idade >= 18:
+print("Maior de idade")
+elif idade >= 16:
+print("Pode votar")
+outro:
+print("Menor de idade")
+
+JavaScript
+se (idade >= 18) {
+console.log("Maior idade");
+} senão se (idade >= 16) {
+console.log("Pode votar");
+} outro {
+console.log("Menor de idade");
+}
+
+Switch Case (JavaScript)
+deixe dia = 2;
+interruptor (dia) {
+caso 1:
+console.log("Domingo");
+quebrar;
+caso 2:
+console.log("Segunda");
+quebrar;
+padrão:
+console.log("Outro dia");
+}
+
+Python
+# para
+para i no intervalo(5):
+imprimir(i)
+# enquanto
+contador = 0
+enquanto contador < 5:
+imprimir(contador)
+contador += 1
+
+JavaScript
+// para
+para (seja i = 0; i < 5; i++) {
+console.log(i);
+}
+// enquanto
+deixe contador = 0;
+enquanto (contador < 5) {
+console.log(contador);
+contador++;
+}
+// faça enquanto
+deixe x = 0;
+fazer {
+console.log(x);
+x++;
+} enquanto (x < 5);
